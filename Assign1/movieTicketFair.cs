c6333dd8d72e20bea1e57d6833d8d6e426c89f81
saikatCV <saikat030703@gmail.com>
2024-03-15 19:26:45 +0530

    the program predict the price of ticket based on diffrent age group

---
+++ Assign1/movieTicketFair.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign1
+{
+    public class Class4
+    {
+        public static void ticket()
+        {
+            Console.WriteLine("Movie ticket counter ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 12)
+            {
+                Console.WriteLine("the ticket price is 100 for children ");
+            }
+            else if (age >= 12 && age <= 64)
+            {
+                Console.WriteLine("the ticket fair is 150 for adults ");
+            }
+            else
+            {
+                Console.WriteLine("the ticket fair is 70 for 65+ age or senior  ");
+            }
+        }
+    }
+}
