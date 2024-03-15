6fafe929bf18358a3552c955064d5df9bda5d175
saikatCV <saikat030703@gmail.com>
2024-03-15 18:55:23 +0530

    created a grade predictor which take input as marks and produce grade as outout

---
+++ Assign1/gradePridictor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign1
+{
+    public class Class8
+    {
+        public static void grade()
+        {
+            Console.WriteLine("Marks predictor ");
+
+            int marks = Convert.ToInt32(Console.ReadLine());
+            marks = marks / 10;
+
+            switch (marks)
+            {
+                case 10:
+                case 9:
+                    Console.WriteLine("Your grade is A");
+                    break;
+
+                case 8:
+                    Console.WriteLine("your grade is B");
+                    break;
+                case 7:
+                    Console.WriteLine("your grade is C");
+                    break;
+                case 6:
+                    Console.WriteLine("your grade is D");
+                    break;
+                default:
+                    Console.WriteLine("your grade is F");
+                    break;
+            }
+        }
+    }
+}
